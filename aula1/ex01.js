8603f8b28ef3db7f6a42d8afc1aae0c49d466065
bruno <bruno.baravelli@escola.pr.gov.br>
2026-06-01 17:48:34 -0300

    primeiro exercício da aula 1

---
+++ aula1/ex01.js
@@ -0,0 +1,11 @@
+let nome = "Bruno";
+let idade = 17;
+let aprovado = true;
+
+console.log(nome);
+console.log(idade);
+console.log(aprovado);
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof aprovado);
